7eb40dd01a575fe1aa8b28d893b78ba0257e8101
Marouane-WEB <MAROUANE.KHARCHI@latoile.em-lyon.com>
2025-12-16 16:00:38 +0100

    partie 3

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-const API_URL =`https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json`;
+const shop1 = document.getElementById("shop");
+const service1 = document.getElementById("service");
+const API_URL = `https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json`;
 
 
-const collectionmain = document.getElementById("Collection");
-const container = document.createElement("div");
-container.classList.add("carte-liste");
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log('Données récupérées avec succès :', data);
 
+        console.log(data.produits);
 
+        displayProducts(data);
 
-  fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-         console.log('Données récupérées avec succès :', data);
-      })
-      
- 
-      data.produits.forEach(element => {
-
-
+    })
+    
+    function displayProducts(data) {
+        let produits = data.produits;
+        produits.forEach(produit => {
+            
+            const article = document.createElement("article");
+            article.classList.add("produits");
+            const title = document.createElement("h3");
+            const descri = document.createElement("p");
+            const image = document.createElement("img");
+            
+            title.textContent = produit.name;
+            descri.textContent = produit.description;
+            console.log(produit['image-url']);
+            console.log(image);
+            
+            image.src = produit["image-url"];
+            
+            
+            
+            
+            article.appendChild(title);
+            article.appendChild(descri);
+            article.appendChild(image);
+            shop1.appendChild(article);
+            
+        });
         
-      });
+    }
+
+       function displayService(data) {
+
+           let services = data.services
+        
+       }
+
+
+    
+    
