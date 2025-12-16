8a7844fd0bb09b47c647dfabdac4f0225f5f70f4
Marouane-WEB <MAROUANE.KHARCHI@latoile.em-lyon.com>
2025-12-16 12:28:53 +0100

    partie2

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+const API_URL =`https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/sneakers.json`;
+
+
 const collectionmain = document.getElementById("Collection");
 const container = document.createElement("div");
 container.classList.add("carte-liste");
 
 
-fetch(API_URL)
+
+  fetch(API_URL)
       .then(response => response.json())
       .then(data => {
          console.log('Données récupérées avec succès :', data);
-
       })
-      .catch(error => console.error('Erreur lors de la récupération des données :', error));
-   //**! FIN FETCH */
+      
+ 
+      data.produits.forEach(element => {
 
 
-
-
-
+        
+      });
